a80e036d8f9442ef6a0459f8c8faf5535cbedd0e
Alondra0221 <113452015+Alondra0221@users.noreply.github.com>
2023-10-24 22:18:28 -0600

    Eliminated image, and some text

--- WidgetApp/ContentView.swift
+++ WidgetApp/ContentView.swift
@@ -10,11 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            Image(systemName: "heart")
             Text("Hello, world!")
-            Text("How Are you?")
         }
         .padding()
     }
